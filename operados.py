2891cdd9c73462ff4757ff52df1c821274ea94c3
Jose M <josemujicanaranjo020895@gmail.com>
2026-02-13 18:21:01 -0500

    Este es mi primer commit

---
+++ operados.py
@@ -0,0 +1,17 @@
+a1 = int(input("Ingresa valor #1:"))
+a2 = int(input("Ingresa valor #2:"))
+a3 = int(input("Ingresa valor #3:"))
+a4 = int(input("Ingresa valor #4:"))
+
+
+multiplicacion1 = a1 * a3
+multiplicacion2 = a2 * a4
+
+division1 = a1 * a3
+division2 = a2 * a4
+
+print("Valor 1 * Valor 3 es igual a Valor 2 * Valor 4: ", multiplicacion1 == multiplicacion2)
+print("Valor 1 * Valor 3 es mayor o igual a Valor 2 * Valor 4: ", multiplicacion1 >= multiplicacion2)
+print("Valor 1 / Valor 3 es igual a Valor 2 / Valor 4: ", division1 == division2)
+print("Valor 1 / Valor 3 es mayor o igual a Valor 2 / Valor 4: ", division1 >= division2)
+
